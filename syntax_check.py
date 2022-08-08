f9b49314be40e6a3ca79171537e1ee9b80450eea
tkabir1 <83760134+tkabir1@users.noreply.github.com>
2022-08-08 09:18:13 -0400

    Create syntax_check.py

---
+++ syntax_check.py
@@ -0,0 +1,12 @@
+import language_tool_python
+tool = language_tool_python.LanguageTool('en-US')
+
+"""text = "Your the best but their are allso  good !"
+matches = tool.check(text)
+print(len(matches))
+print(matches)"""
+
+text = "You are the best but they are also good !"
+matches = tool.check(text)
+print(len(matches))
+print(matches)
