d0d7f863e393ebc4c4259fbe7735ce28ba1b752e
tkabir1 <83760134+tkabir1@users.noreply.github.com>
2022-10-14 14:54:15 -0400

    Update syntax_check.py

--- syntax_check.py
+++ syntax_check.py
@@ -1,12 +1,26 @@
 import language_tool_python
 tool = language_tool_python.LanguageTool('en-US')
+def syntax_checker(text):
+    """text = "Your the best but their are allso  good !"
+    matches = tool.check(text)
+    print(len(matches))
+    print(matches)"""
+    matches = tool.check(text)
+    #print(len(matches))
+    #print("suggestion:",matches)
+    return (tool.correct(text))
+def is_quote_ok(s):
+    stack = []
+    for c in s:
+        if c in [ '"', "`"]:
+            if stack and stack[-1] == c:
+                # this single-quote is close character
+                stack.pop()
+            else:
+                # a new quote started
+                stack.append(c)
+        else:
+            # ignore it
+            pass
 
-"""text = "Your the best but their are allso  good !"
-matches = tool.check(text)
-print(len(matches))
-print(matches)"""
-
-text = "You are the best but they are also good !"
-matches = tool.check(text)
-print(len(matches))
-print(matches)
+    return len(stack) == 0
