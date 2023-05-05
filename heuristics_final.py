fa13bba74fc38635d44fce90f11afc945a21755d
tkabir1 <83760134+tkabir1@users.noreply.github.com>
2023-05-05 15:15:05 -0400

    Update heuristics_final.py

--- heuristics_final.py
+++ heuristics_final.py
@@ -10,44 +10,31 @@ from collections import Iterable
 
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
 from nltk.corpus import wordnet as wn
 from nltk import Tree
-from nq_condition3 import *
 
 import spacy
-import neuralcoref
+
+from nq_condition2 import *
 
 nlp = spacy.load('en_core_web_sm')
-neuralcoref.add_to_pipe(nlp)
-
-def to_nltk_tree(node):
-    if node.n_lefts + node.n_rights > 0:
-        return Tree(node.orth_, [to_nltk_tree(child) for child in node.children])
-    else:
-        return node.orth_
-
 
 # Heuristics for NQlike quality checking
 class HeuristicsTransformer:
   def __init__(self, config, lat_lookup):
     self.current_analysis = {}    
-    self.valid_verbs = config["valid_verbs"]
-    self.wh_words = config["wh_words"]
-    self.strictly_valid_verbs = config["strictly_valid_verbs"]
-    self.to_trim = config["to_trim"]
-    self.pos_pronouns = config["pos_pronouns"]
-    self.pronouns = config["pronouns"]
-    self.imperative_pattern = {re.compile(x) for x in config["imperative"]}
-    self.regexp_trims = dict((re.compile(x), y) for x, y in config["remove_dict"].items())
-    self.non_last_sent_transform_dict = config["non_last_sent_transform_dict"]
+
     self.answer_type_dict = lat_lookup
-    self.heuristics_ = [split_conjunctions("Split Connjunctions"),imperative_to_question("imperative"),no_wh_words("no_wh")]
+
+    self.heuristics_ = [split_conjunctions("Split Connjunctions", config),
+                          imperative_to_question("Imperative to Question", config),
+                          no_wh_words("No WH words", config)]
 
   def cache_analysis(self, question, verbose=False):
     """
     Parse the sentence for downstream heuristics.  This prevents repeated computation in each of the heuristics.
     """
-    print("code in cachex")
     tokens = nltk.word_tokenize(question.lower())
     text = nltk.Text(tokens)
     tagged = nltk.pos_tag(text)
@@ -56,24 +43,26 @@ class HeuristicsTransformer:
     if verbose:
       for ii in spacy_parse.sents:
         logging.debug(to_nltk_tree(ii.root))
-          
-    self.current_analysis[question] = {"spacy": spacy_parse, "nltk_tokens": tokens, "nltk_tags": tagged}
-    print(self.current_analysis[question]["spacy"])
+
+    parses = {"spacy": spacy_parse, "nltk_tokens": tokens, "nltk_tags": tagged}
+    self.current_analysis[question] = parses
+    for ii in self.heuristics_:
+      ii.add_analysis(question, parses)
 
     
   def __call__(self, qb_id, answer, chunk_id, question, lexical_answer_phrase, question_determiner, suppress_errors=False):
     self.current_analysis = {}
+    self.cache_analysis(question)
+
     finished = set()
     applied_transformations = []
-    self.cache_analysis(question)
 
     while len(finished) < len(self.current_analysis):
       unchecked = [x for x in self.current_analysis if x not in finished]
       for qq in unchecked:
         assert not qq in finished
         for method in self.heuristics_:
-          #logging.debug("Applying method %s" % method.name)
-
+          logging.debug("Applying method %s" % method.name)
           if suppress_errors:
             try:
               results = method(qb_id, qq, lexical_answer_phrase, question_determiner)
@@ -87,7 +76,8 @@ class HeuristicsTransformer:
 
           for new_question in results:
               if new_question != qq:
-                #logging.debug("Adding new question [%s]: %s" % (method_name, new_question))
+                logging.debug("Adding new question [%s]: %s" % (method.name, new_question))
+                self.cache_analysis(new_question)
 
                 row = {}
                 row["qanta_id"] = qb_id
@@ -96,11 +86,13 @@ class HeuristicsTransformer:
                 row["parent"] = qq
                 row["chunk_id"] = chunk_id
                 row["question"] = new_question.lower()
-                #row["transform"] = method_name
+                row["transform"] = method.name
                 applied_transformations.append(row)
                 
                 self.cache_analysis(new_question)
-          finished.add(qq)
+
+          if method.replace:
+            finished.add(qq)
 
 
     self.current_analysis = None
@@ -114,6 +106,8 @@ if __name__ == "__main__":
                       help="path of the qb dataset")
   parser.add_argument('--config_file', type=str, default='config.json',
                       help="File with data that configures extraction")
+  parser.add_argument('--output_file', type=str, default='./qanta_heuristic_output.json',
+                      help="File with data that configures extraction")
 
   args = parser.parse_args()
   # Load dataset
@@ -122,33 +116,59 @@ if __name__ == "__main__":
     config = json.load(json_file)
 
   with open(args.qb_path) as json_file:
-    questions = json.load(json_file)["questions"]
+    questions_full = json.load(json_file)
 
-  print(questions)
+  example=[]
+  qid=[]
+  exs=[]
+  for i in questions_full:
+    question=i['text']
+    qid_i=i['qanta_id']
+    answer=i["answer"]
+    question_split=sent_tokenize(question)
+    for j in range(len(question_split)):
+      e1=tuple((qid_i,answer,question_split[j]))
+      exs.append(e1)
+      #qid.append(qid_i)
+      #example.append(question_split[i])
+    #print(question)
   
   heuristics = HeuristicsTransformer(config, {0: "character", 1: "thing", 94: "ruler", 102: "novel", 104: "organ"})
   logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-  #example
 
-  for qid, example in [(102, "An automobile in what novel was purchased in Fuller by Bessie for her marriage to 16-year - old Dude , and led to the death of both an African - American man and Grandmother Lester ."),
-                       (94, "He demanded compensation for the family of Jacob Kaiser and forced another group to end its alliance with Austria in an armistice that he negotiated to end a war in which no battles occurred."),
-                       (0, ' what character is first encountered in the Spouter - Inn where the landlord thinks he may be late because " he ca n\'t sell his head , " and his coffin helps save the narrator after the ship he \'s on sinks . \xa0'),
-                       (19, "The protagonist of one of who 's works gives a jar to his friend instead of repaying a loan , and later dies after embezzling money in an attempt to buy out a courtesan 's contract ."),
-                           (1, "'!$  % ##@# @@# ftp FTP ftp--- For 10 points, for 10 points , For 10 points --- for 20 points: for 10 points--this is a real, legit question !@#!@@#@%%%....?"),
-                           (104, "For 10 points , name this organ , home to the islets of Langerhans ."),
-                           (8, "For 10 points , identify this French government that succeeded the Second Empire ."),
-                           (98, "For 10 points , identify this Sanskrit - language author of \" The Cloud Messenger \" and The Recognition of Shakuntala ."),
-                           (59, "For 10 points , name this mortal lover of Zeus and mother of Dionysus ."),
-                           (20, "For 10 points , name this Italian author of The Name of the Rose and Foucault 's Pendulum ."),
-                           (17, "For 10 points , name this country , the home of the director of The Seventh Seal , Ingmar Bergman ."),
-                           (119, "For 10 points , name this 1859 essay condemning infringements on personal freedom , written by John Stuart Mill ."),
-                           (378, "Beginning with \" April is the cruellest month \" , for 10 points , name this long poem in five sections by T.S. Eliot ."),
-                           (361,  " who asserted \" the air is a spongy body / a promiscuous faceless being \" in \" The Balcony \" and asked \" Do I believe in man / or in the stars ? \" in another poem .")]:
-
-      transformations = heuristics(qid, "??", 0, example, "what thing", "what")
+  f1 = open(args.output_file, "w")
+  f1.write("[")
+  for qid, answer,example in exs:
+      doc=nlp(example)
+      delt=" "
+      for i in doc.noun_chunks:
+        if i.text.lower().find("this ")!=-1:
+          delt=i.text.lower().replace("this ","which ")
+          break
+        elif i.text.lower().find("these ")!=-1:
+          delt=i.text.lower().replace("these ","which ")
+          break
+        for j in doc:
+          if j.text.lower()=='he':
+            delt=j.text.lower().replace("he","who")
+            break
+          elif j.text.lower()=='she':
+            delt=j.text.lower().replace("she","who")
+            break
+          elif j.text.lower()=='they':
+            delt=j.text.lower().replace("they","what")
+            break
+        break
+      print("replaced text: ",delt)
+      transformations = heuristics(qid, answer, 0, example, "what thing", delt)
+      json.dump(transformations,f1,indent=4)
+      f1.write(",")
       for transform in transformations:
         logging.debug("===============================")
         logging.debug(transform["original"])
         logging.debug(transform["parent"])
-        #logging.debug(transform["transform"])           
+        logging.debug(transform["transform"])           
         logging.debug(transform["question"])
+        print(transform["question"])
+  f1.write("]")
+  f1.close()
